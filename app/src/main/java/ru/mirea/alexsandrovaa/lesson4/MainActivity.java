2700a84cfc138b05afba2418ab4d6e0aec542d4d
Andrey Alexsandrov <an.alex25@mail.ru>
2025-05-05 17:50:46 +0300

    Практика 4 | Задание 1 | Плеер

--- app/src/main/java/ru/mirea/alexsandrovaa/lesson4/MainActivity.java
+++ app/src/main/java/ru/mirea/alexsandrovaa/lesson4/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.mirea.alexsandrovaa.lesson4;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import ru.mirea.alexsandrovaa.lesson4.databinding.ActivityMainBinding ;
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding; // создаём переменную для ViewBinding
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Подключаем разметку через ViewBinding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Устанавливаем текст
+        binding.songTitle.setText("Imagine");
+        binding.artistName.setText("John Lennon");
     }
 }
